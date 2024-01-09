6aecbb6ccf2a54a1b7c7f2d56ec6daf9a5775c37
nggigi <ngigi.francis5@gmail.com>
2024-01-09 03:08:18 -0500

    done

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+
+let num1=31;
+let num2=2;
+let multiply=num1*num2
+
+let random= Math.floor(Math.random()*3)+1
+
+let num3=25
+let num4=7
+let mod= num3 % num4
+
+let max=Math.max(10, 20, 20, 13, 14)
